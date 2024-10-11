2f2e3973f9d1d732662e189027762437ca39b9ee
omprakash gutta <prakashgutta186@gmail.com>
2024-10-11 19:06:18 +0530

    Revert "here we gpo for @Restcontroller"
    This reverts commit 78d9ec2071c7ec74f4ea7f93147fc646a307d53d.

--- src/main/java/com/Quiz/Controller/QuizController.java
+++ src/main/java/com/Quiz/Controller/QuizController.java
@@ -1,9 +1,4 @@
 package com.Quiz.Controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("question")
 public class QuizController {
 }
